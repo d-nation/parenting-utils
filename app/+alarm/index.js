283b810964a9dabbd01d77c1e0519035d8f2fd9c
Dustin Nation <d.r.nation@gmail.com>
2016-07-15 19:11:56 -0500

    Removes extras from accidental gh-pages merge into master :o oops

--- app/+alarm/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var alarm_component_1 = require('./alarm.component');
-exports.AlarmComponent = alarm_component_1.AlarmComponent;
-var alarm_service_1 = require('./alarm.service');
-exports.AlarmService = alarm_service_1.AlarmService;
-//# sourceMappingURL=../../index.js.map
